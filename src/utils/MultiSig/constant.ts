af4e6b1c111a164fcf3e27657ce9b3e811895939
JoeHo888 <joe.hochotai@gmail.com>
2020-04-13 21:53:00 +0800

    Add constant for MultiSig Utils

---
+++ src/utils/MultiSig/constant.ts
@@ -0,0 +1,8 @@
+const compressedPublicKeyPrefix_1: string = "02";
+const compressedPublicKeyPrefix_2: string = "03";
+
+
+export {
+    compressedPublicKeyPrefix_1,
+    compressedPublicKeyPrefix_2,
+};
