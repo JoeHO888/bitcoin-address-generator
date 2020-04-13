1c5ce5c3c9771791d9b144d59923e0650d8a5907
JoeHo888 <joe.hochotai@gmail.com>
2020-04-13 21:33:15 +0800

    Make Header Home Button functional

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from "react-router-dom";
 import HomeIcon from '@material-ui/icons/Home';
 import IconButton from '@material-ui/core/IconButton';
 
 const Header: React.FC = () => {
+
     return (
         <div>
             <AppBar position="relative">
                 <Toolbar>
-                    <IconButton color="inherit">
+                    <IconButton component={Link} to={'/'} color="inherit">
                         <HomeIcon />
                     </IconButton>
                     <Typography variant="h6" color="inherit" noWrap>
