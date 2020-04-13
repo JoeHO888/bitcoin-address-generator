040afe4ff70b5d0ff86d8433a649550be9b69f8c
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 06:39:23 +0800

    Add Signature Amount Validation for MultiSig Utils

--- src/utils/MultiSig/validate.ts
+++ src/utils/MultiSig/validate.ts
@@ -16,6 +16,7 @@ const isValidPublicKey = (pubKey: Buffer): boolean => {
     return ecc.isPoint(pubKey);
 }
 
+
 type publicKeyValidation = {
     valid: boolean;
     error?: string;
@@ -36,6 +37,27 @@ const isValidCompressedPublicKey = (pubKey: Buffer): publicKeyValidation => {
     return validationResult;
 }
 
+type signatureAmountValidation = {
+    valid: boolean;
+    error?: string;
+}
+
+const isAmountSignatureProper = (m: number, pubKeys: string[]): signatureAmountValidation => {
+    const validationResult: signatureAmountValidation = { valid: true };
+
+    if (m < 1 && m > pubKeys.length) {
+        validationResult.valid = false
+        validationResult.error = "Amount of Signature should be less than total number of public keys."
+    }
+
+    if (m < 1 && m > 20) {
+        validationResult.valid = true
+        validationResult.error = "Amount of Signature should be less than 20."
+    }
+    return validationResult;
+}
+
 export {
     isValidCompressedPublicKey,
+    isAmountSignatureProper
 }
