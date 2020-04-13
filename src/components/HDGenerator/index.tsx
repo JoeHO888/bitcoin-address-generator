a1931e2ebdfeb65f9fed00f4940bc3a10590369a
JoeHo888 <joe.hochotai@gmail.com>
2020-04-13 21:29:10 +0800

    Initialize HDGenerator Component

---
+++ src/components/HDGenerator/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HDGenerator: React.FC = () => {
+
+    return (
+        <>
+            <h1>
+                HDGenerator
+            </h1>
+        </>
+    )
+}
+
+export { HDGenerator }
