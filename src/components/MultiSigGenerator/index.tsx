9be3aaf518a938f3f7274312bac674c18e436c94
JoeHo888 <joe.hochotai@gmail.com>
2020-04-13 21:29:25 +0800

    Initialize MultiSig Component

---
+++ src/components/MultiSigGenerator/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MultiSigGenerator: React.FC = () => {
+
+    return (
+        <>
+            <h1>
+                MultiSigGenerator
+            </h1>
+        </>
+    )
+}
+
+export { MultiSigGenerator }
