3b907f552db3b25a2d0b15f054dbe08e2b971259
JoeHo888 <joe.hochotai@gmail.com>
2020-04-13 21:30:02 +0800

    Add Route to different components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import './App.css';
-
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { HDGenerator } from "./components/HDGenerator";
+import { Home } from "./components/Home";
+import { MultiSigGenerator } from "./components/MultiSigGenerator";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Switch>
+            <Route exact path="/multisig-generator">
+              <MultiSigGenerator />
+            </Route>
+            <Route exact path="/hd-generator">
+              <HDGenerator />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
 
