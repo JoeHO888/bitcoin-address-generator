162e6b5eac946a41babaf47c3bb351204aa7d2c0
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 19:31:08 +0800

    Add Hexadecimal Test Suite

---
+++ src/utils/Common/hexadecimal.spec.ts
@@ -0,0 +1,41 @@
+import { isStringHexadecimal } from "./hexadecimal";
+
+describe('Hexadecimal', () => {
+    test('Odd Number Length Fail', () => {
+        const expectedRes = false
+        const string = "1";
+
+        const res = isStringHexadecimal(string)
+
+        expect(res).toStrictEqual(expectedRes);
+    });
+
+    test('Non 0-9 & a-f characters Fail', () => {
+        const expectedRes = false
+        const string = "z123";
+
+        const res = isStringHexadecimal(string)
+
+        expect(res).toStrictEqual(expectedRes);
+    });
+
+    test('Arbitrary String Fail', () => {
+        const expectedRes = false
+        const string = "213 fsdg";
+
+        const res = isStringHexadecimal(string)
+
+        expect(res).toStrictEqual(expectedRes);
+    });    
+
+    test('Hexadecimal - Success', () => {
+        const expectedRes = true
+        console.log(expectedRes);
+        const string = "ff";
+
+        const res = isStringHexadecimal(string)
+        console.log(res);
+
+        expect(res).toStrictEqual(expectedRes);
+    });  
+})
