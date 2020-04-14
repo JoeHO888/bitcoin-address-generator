b74d6ac2207bd3cb99c649ada6a289867dc42052
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 15:38:06 +0800

    Refactor HDGenerator

--- src/components/HDGenerator/index.tsx
+++ src/components/HDGenerator/index.tsx
@@ -49,95 +49,99 @@ const HDGenerator: React.FC = () => {
         if (!seedValidation.valid) {
             setSeedHasError(true);
             setSeedHelperText((seedValidation.error) as string)
+            return
 
-        }// Valid Path
-        else if (!pathValidation.valid) {
+        }
+        // Valid Path
+        if (!pathValidation.valid) {
             setPathHasError(true);
             setPathHelperText((pathValidation.error) as string)
-            // Try to Derive Child Key
-        } else {
-            const derivedAddress = deriveHDSegWitAddress(seed, path);
+            return
 
-            // Invalid Child Key
-            if (!derivedAddress.address) {
-                setAddressHasError(true)
-                setAddressHelperText((derivedAddress.error) as string)
-            }
-
-            setAddress((derivedAddress.address) as string);
         }
+        // Try to Derive Child Key
+        const derivedAddress = deriveHDSegWitAddress(seed, path);
+
+        // Invalid Child Key
+        if (!derivedAddress.address) {
+            setAddressHasError(true)
+            setAddressHelperText((derivedAddress.error) as string)
+            return
+        }
+
+        setAddress((derivedAddress.address) as string);
     }
 
-    return (
-        <div className="generator-form">
-            <h1>
-                {routes.hdSegWit.title}
-            </h1>
-            <h2>
-                {routes.hdSegWit.description}
-            </h2>
-            <h3>
-                <a href={routes.hdSegWit.methodologyURL} target="_blank" rel="noopener noreferrer">
-                    How is it generated?
+return (
+    <div className="generator-form">
+        <h1>
+            {routes.hdSegWit.title}
+        </h1>
+        <h2>
+            {routes.hdSegWit.description}
+        </h2>
+        <h3>
+            <a href={routes.hdSegWit.methodologyURL} target="_blank" rel="noopener noreferrer">
+                How is it generated?
                 </a>
-            </h3>
-            <form autoComplete="off">
-                <Grid container className="form-field">
-                    <Grid item xs={12}>
-                        <TextField
-                            error={seedHasError}
-                            placeholder="Seed to generate HD address"
-                            helperText={seedHelperText}
-                            fullWidth
-                            value={seed}
-                            onChange={handleSeedChange}
-                            margin="normal"
-                            id="seed"
-                            label="Seed" />
-                    </Grid>
+        </h3>
+        <form autoComplete="off">
+            <Grid container className="form-field">
+                <Grid item xs={12}>
+                    <TextField
+                        error={seedHasError}
+                        placeholder="Seed to generate HD address"
+                        helperText={seedHelperText}
+                        fullWidth
+                        value={seed}
+                        onChange={handleSeedChange}
+                        margin="normal"
+                        id="seed"
+                        label="Seed" />
                 </Grid>
+            </Grid>
 
-                <Grid container className="form-field">
-                    <Grid item xs={12}>
-                        <TextField
-                            error={pathHasError}
-                            placeholder="Example: m/123'/456"
-                            helperText={pathHelperText}
-                            fullWidth
-                            value={path}
-                            onChange={handlePathChange}
-                            margin="normal"
-                            id="path"
-                            label="Path" />
-                    </Grid>
+            <Grid container className="form-field">
+                <Grid item xs={12}>
+                    <TextField
+                        error={pathHasError}
+                        placeholder="Example: m/123'/456"
+                        helperText={pathHelperText}
+                        fullWidth
+                        value={path}
+                        onChange={handlePathChange}
+                        margin="normal"
+                        id="path"
+                        label="Path" />
                 </Grid>
+            </Grid>
 
-                <Grid className="form-field" container alignItems="flex-start" justify="flex-end" direction="row">
-                    <Grid item>
-                        <Button
-                            type="submit"
-                            variant="outlined"
-                            color="primary"
-                            onClick={onSubmit}>
-                            Submit
+            <Grid className="form-field" container alignItems="flex-start" justify="flex-end" direction="row">
+                <Grid item>
+                    <Button
+                        type="submit"
+                        variant="outlined"
+                        color="primary"
+                        onClick={onSubmit}>
+                        Submit
                 </Button>
 
-                    </Grid>
                 </Grid>
-                <Grid container className="form-field">
-                    <Grid item xs={12}>
-                        <TextField
-                            error={addressHasError}
-                            disabled
-                            helperText={addressHelperText}
-                            fullWidth
-                            value={address}
-                            margin="normal" id="address" label="HD P2WPKH Address" />
-                    </Grid>
+            </Grid>
+            <Grid container className="form-field">
+                <Grid item xs={12}>
+                    <TextField
+                        error={addressHasError}
+                        disabled
+                        helperText={addressHelperText}
+                        fullWidth
+                        value={address}
+                        margin="normal" id="address" label="HD P2WPKH Address" />
                 </Grid>
-            </form>
-        </div>
-    )
+            </Grid>
+        </form>
+    </div>
+)
 }
 
 export { HDGenerator };
