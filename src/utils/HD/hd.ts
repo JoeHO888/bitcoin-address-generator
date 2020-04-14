87410c09e182c68e5e2aaf689d3e5456fa93f356
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 10:41:48 +0800

    Initialize Skeleton for DeriveHDSegWitAddress

---
+++ src/utils/HD/hd.ts
@@ -0,0 +1,19 @@
+const deriveHDSegWitAddress = (seed: string, derivedPath: string) => {
+
+    // check path valid
+
+    // Check this seed can generate valid master key
+
+    // Create Master Node
+
+    // Parse Path
+
+
+    // Derive Node
+
+    return
+}
+
+export {
+    deriveHDSegWitAddress
+}
