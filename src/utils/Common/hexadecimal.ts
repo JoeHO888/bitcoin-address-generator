8c86e38f2962d28d74856846cc5dd301ace8d78e
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 19:30:46 +0800

    Fix Regex in isStringHexadecimal

--- src/utils/Common/hexadecimal.ts
+++ src/utils/Common/hexadecimal.ts
@@ -1,6 +1,6 @@
 const isStringHexadecimal = (string: string): boolean => {
     const lowerCaseString = string.toLowerCase();
-    const pattern = new RegExp(/([0-9a-f])*$/, "i")
+    const pattern = new RegExp(/([0-9a-f])+$/, "i")
     return !isNaN(parseInt(lowerCaseString, 16))
         && lowerCaseString.length % 2 === 0
         && pattern.test(lowerCaseString);
