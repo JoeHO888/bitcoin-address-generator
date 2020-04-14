41940464fcfb8da6310c2e3140fe25ae721d89fe
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 08:37:25 +0800

    Update Text in HDGenerator

--- src/components/HDGenerator/index.tsx
+++ src/components/HDGenerator/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { routes } from "../../routes";
 
 const HDGenerator: React.FC = () => {
 
     return (
         <>
             <h1>
-                HDGenerator
+                {routes.hdSegWit.title}
             </h1>
+            <h2>
+                {routes.hdSegWit.description}
+            </h2>
+            <h3>
+                <a href={routes.hdSegWit.methodologyURL} target="_blank">
+                    How is it generated?
+                </a>
+            </h3>
         </>
     )
 }
