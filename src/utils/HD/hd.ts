475f0df0df6f582945ca5372f11200dd1234e7de
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 10:45:29 +0800

    Check Master Node Validity in DerivedHDSegWitAddress

--- src/utils/HD/hd.ts
+++ src/utils/HD/hd.ts
@@ -1,11 +1,27 @@
+import * as ecc from "tiny-secp256k1";
 import { isDerivedPathValid } from "./validate"
+import { BIP32, generate_master_key } from "./bip32";
+
+const isMasterKeyPairValid = (masterExtendedKeyPair: [Buffer, Buffer]): boolean => {
+    const IL = masterExtendedKeyPair[0]
+    return ecc.isPrivate(IL);
+}
 
 const deriveHDSegWitAddress = (seed: string, derivedPath: string) => {
 
     // check path valid
     const derivedPathValidation = isDerivedPathValid(derivedPath);
-    
+
     // Check this seed can generate valid master key
+    const masterExtendedKeyPair = generate_master_key(seed);
+
+    if (!isMasterKeyPairValid(masterExtendedKeyPair)) {
+
+        // Create error message
+
+        // Return null node
+
+    }
 
     // Create Master Node
 
