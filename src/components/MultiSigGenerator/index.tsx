d853c6c009246d3d4d5782456ec452d3e5554975
JoeHo888 <joe.hochotai@gmail.com>
2020-04-14 08:37:35 +0800

    Update Text in MultiSigGenerator

--- src/components/MultiSigGenerator/index.tsx
+++ src/components/MultiSigGenerator/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { routes } from "../../routes";
 
 const MultiSigGenerator: React.FC = () => {
 
     return (
         <>
             <h1>
-                MultiSigGenerator
+                {routes.multiSig.title}
             </h1>
+            <h2>
+                {routes.multiSig.description}
+            </h2>
+            <h3>
+                <a href={routes.multiSig.methodologyURL} target="_blank">
+                    How is it generated?
+                </a>
+            </h3>
         </>
     )
 }
